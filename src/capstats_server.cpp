a18af4639e0970fc87a9f8d48ec59015bd2d9e9a
Gus Smith <hfs5022@psu.edu>
2016-10-24 23:24:52 -0400

    not compiling due to a problem with otl

--- src/capstats_server.cpp
+++ src/capstats_server.cpp
@@ -2,8 +2,20 @@
 #include <cstdlib>
 #include <restbed>
 
+#define OTL_ODBC // Compile OTL 4.0/ODBC
+#define ODBCVER 0x0250 // ODBC Version # needs to be downgraded
+// to 2.5 because the SQLite ODBC driver seems
+// to run slower when ODBC 3.x functions
+// used (performance is not as good as with
+// ODBC 2.5 function calls)
+#include <otlv4.h> // include the OTL 4.0 header file
+
+otl_connect db; // connect object
+
 using namespace std;
 using namespace restbed;
+
+
 
 void post_method_handler( const shared_ptr< Session > session )
 {
@@ -20,6 +32,21 @@ void post_method_handler( const shared_ptr< Session > session )
 
 int main( const int, const char** )
 {
+	otl_connect::otl_initialize();
+	try {
+		db << ":memory:";
+		try { db << "drop table test_tab"; }
+		catch (otl_exception&) {}
+		db << "create table test_tab(f1 int, f2 varchar(30))";
+
+	}
+
+	catch (otl_exception& p) { // intercept OTL exceptions
+		cerr << p.msg << endl; // print out error message
+		cerr << p.stm_text << endl; // print out SQL that caused the error
+		cerr << p.var_info << endl; // print out the variable that caused the error
+	}
+
     auto resource = make_shared< Resource >( );
     resource->set_path( "/resource" );
     resource->set_method_handler( "POST", post_method_handler );

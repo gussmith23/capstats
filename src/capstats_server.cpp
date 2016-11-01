4ebb10bc50f73eeac6583c41c0e7a1314033fcb8
Gus Smith <hfs5022@psu.edu>
2016-10-31 23:28:36 -0400

    adds jsonbox

--- src/capstats_server.cpp
+++ src/capstats_server.cpp
@@ -12,7 +12,11 @@
 
 #include "include_otl.h"
 
+#include "player_dao.h"
+
 otl_connect db; // connect object
+
+PlayerDAO playerDAO;
 
 using namespace std;
 using namespace restbed;
@@ -32,6 +36,20 @@ void post_method_handler( const shared_ptr< Session > session )
 
 int main( const int, const char** )
 {
+	// Setup db.
+	otl_connect::otl_initialize();
+	try {
+
+		db << "DRIVER=SQLite3 ODBC Driver;Database=test.db;"; 
+		playerDAO.init();
+	}
+
+	catch (otl_exception& p) { // intercept OTL exceptions
+		cerr << p.msg << endl; // print out error message
+		cerr << p.stm_text << endl; // print out SQL that caused the error
+		cerr << p.var_info << endl; // print out the variable that caused the error
+	}
+
     auto resource = make_shared< Resource >( );
     resource->set_path( "/resource" );
     resource->set_method_handler( "POST", post_method_handler );

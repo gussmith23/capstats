6f4de6343431da82a282df6166c81b8a28f05bd9
Gus Smith <hfs5022@psu.edu>
2016-11-02 22:13:47 -0400

    adds get and post

--- src/capstats_server.cpp
+++ src/capstats_server.cpp
@@ -10,7 +10,10 @@
 #include <sqltypes.h>
 #include <sqlext.h>
 
+#include <string>
+
 #include "include_otl.h"
+#include "JsonBox.h"
 
 #include "player_dao.h"
 
@@ -20,8 +23,9 @@ PlayerDAO playerDAO;
 
 using namespace std;
 using namespace restbed;
+using namespace JsonBox;
 
-void post_method_handler( const shared_ptr< Session > session )
+void post_user_handler( const shared_ptr< Session > session )
 {
     const auto request = session->get_request( );
 
@@ -31,11 +35,47 @@ void post_method_handler( const shared_ptr< Session > session )
     {
         fprintf( stdout, "%.*s\n", ( int ) body.size( ), body.data( ) );
         session->close( OK, "Hello, World!", { { "Content-Length", "13" } } );
-    } );
+
+		Value player;
+		player.loadFromString(string(body.begin(), body.end()));
+
+		playerDAO.addPlayer(Player(player["telegramId"].getInteger(), player["name"].getString()));
+		
+		session->close(OK, "", { { "Content-Length", "0" },{ "Content-Type", "application/json" } });
+
+	} );
 }
 
-void post_add_user_handler(const shared_ptr<Session> session)
+void get_user_handler(const shared_ptr<Session> session)
 {
+	const auto request = session->get_request();
+	
+	const unsigned int telegramId = stoi(request->get_query_parameter("telegramId"));
+
+	Player player = playerDAO.getPlayer(telegramId);
+
+	Object playerJson;
+	playerJson["name"] = Value(player.getName());
+	playerJson["telegramId"] = Value((int)player.getTelegramId());
+
+	Object out;
+	out["player"] = playerJson;
+
+	stringstream stream;
+	Value(out).writeToStream(stream);
+	string response_body = stream.str();
+
+	session->close(OK, response_body, { { "Content-Length", to_string(response_body.length()) },{ "Content-Type", "application/json" } });
+}
+
+void get_user_http(const shared_ptr<Session> session)
+{
+	const auto request = session->get_request();
+	const unsigned int telegramId = stoi(request->get_query_parameter("telegramId"));
+
+	Player player = playerDAO.getPlayer(telegramId);
+
+	string out_html = "<h1>" + player.getName() + "</h1><p><b>Telegram ID:</b>" + "</p>";
 
 }
 
@@ -54,17 +94,19 @@ int main( const int, const char** )
 		cerr << p.stm_text << endl; // print out SQL that caused the error
 		cerr << p.var_info << endl; // print out the variable that caused the error
 	}
+	
+	auto user = make_shared<Resource>();
+	user->set_path("/player");
+	user->set_method_handler("GET", get_user_handler);
+	user->set_method_handler("POST", post_user_handler);
 
-    auto resource = make_shared< Resource >( );
-    resource->set_path( "/resource" );
-    resource->set_method_handler( "POST", post_method_handler );
 
     auto settings = make_shared< Settings >( );
     settings->set_port( 23232 );
     settings->set_default_header( "Connection", "close" );
 
     Service service;
-    service.publish( resource );
+	service.publish(user);
     service.start( settings );
 
     return EXIT_SUCCESS;

73efb298747af26be97d8748ea1bf2f8e2c8d2f8
Gus Smith <hfs5022@psu.edu>
2016-11-26 21:57:48 -0500

    removes unneeded files

--- test/sendget.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = 'http://localhost:23232/player' # Set destination URL here
-get_params = {'telegramId': '1'}
-
-r = requests.get(url, params=get_params)
-print(r.json())
--- test/sendpost.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = 'http://localhost:23232/game' # Set destination URL here
-
-post_params = {'teams': { 1 : [1,2,3], 2: [4,5,6] } }
-r = requests.post(url, json = post_params)
-

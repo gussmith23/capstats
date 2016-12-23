7bc9e9e3c4cb7a3402363242af57e821e43c46ca
Gus Smith <gushenrysmith@gmail.com>
2016-12-22 19:13:18 -0500

    handles null better

--- src/player_dao.cpp
+++ src/player_dao.cpp
@@ -23,9 +23,15 @@ bool PlayerDAO::addPlayer(Player &player) const
       "(:name<char[100]>, :telegramId<long>, :telegramUsername<char[100]>)",
       *db);
 
-  o	<< player.getName()
-    << player.getTelegramId()
-    << player.getTelegramUsername();
+  if (player.getName().size() > 0) o << player.getName();
+  else o << otl_null();
+
+  if (player.getTelegramId() >= 0) o << player.getTelegramId();
+  else o << otl_null();
+
+  if (player.getTelegramUsername().size() > 0) o << player.getTelegramUsername(); 
+  else o <<otl_null();
+
   o.flush();
 
   otl_stream lastRowidStream(
@@ -47,10 +53,16 @@ Player PlayerDAO::getPlayer(long id) const
       "select name, telegramId, telegramUsername from players where rowid=:id<long>", 
       *db);
   o << id;
+
+  if (o.eof()) return Player(-1);
+
   string name; long telegramId; string telegramUsername;
-  if (!o.eof())
-    o >> name >> telegramId >> telegramUsername;
-  else return Player(-1);
+  o >> name;
+  if (o.is_null()) name = "";
+  o >> telegramId;
+  if (o.is_null()) telegramId = -1;
+  o >> telegramUsername;
+  if (o.is_null()) telegramUsername = "";
 
   Player out(id);
   out.setName(name);
@@ -63,28 +75,23 @@ Player PlayerDAO::getPlayer(long id) const
 Player PlayerDAO::findPlayerByTelegramUsername(const string& telegramUsername)
 {
   otl_stream o(50,
-      "select name, telegramId, rowid from players where telegramUsername=:telegramUsername<char[100]>",
+      "select rowid from players where telegramUsername=:telegramUsername<char[100]>",
       *db);
   o << telegramUsername;
-  string name; long telegramId; long id;
-  if (!o.eof())
-    o >> name >> telegramId >> id;
-  else return Player(-1);
 
-  Player out;
-  out.setId(id);
-  out.setName(name);
-  out.setTelegramId(telegramId);
-  out.setTelegramUsername(telegramUsername);
+  if (o.eof()) return Player(-1);
 
-  return out;
+  long id;
+  o >> id;
+
+  return getPlayer(id);
 }
 
 std::vector<Player> PlayerDAO::findPlayers(long id, const std::string & name, 
     long telegramId, const std::string & telegramUsername)
 {
   stringstream selectStringBase;
-  selectStringBase << "select name, telegramId, telegramUsername, rowid ";
+  selectStringBase << "select rowid ";
   selectStringBase << "from players ";
 
   vector<string> whereClauses;
@@ -111,19 +118,11 @@ std::vector<Player> PlayerDAO::findPlayers(long id, const std::string & name,
   if (telegramUsername != "") select << telegramUsername;
 
   vector<Player> out;
-  while (!select.eof()) {
-    long id_out = -1; 
-    string name_out = "";
-    long telegramId_out = -1;
-    string telegramUsername_out = "";
-
-    select >> name_out >> telegramId_out >> telegramUsername_out >> id_out;
-
-    Player p;
-    p.setId(id_out); p.setName(name_out); p.setTelegramId(telegramId_out);
-    p.setTelegramUsername(telegramUsername_out);
-
-    out.push_back(p);
+  while (!select.eof()) 
+  {
+    long id; 
+    select >> id;
+    out.push_back(getPlayer(id));
   }
 
   return out;

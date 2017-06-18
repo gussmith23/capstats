739801ab3f8fcfafed1d499231ba5e3d395059f1
Gus Smith <guscomps@gmail.com>
2017-06-18 23:09:07 +0000

    Revert "cleans up restbed dependency. expects user to just install it themselves"
    This reverts commit 4212c24fa8f577a8af7d6469b0b946accc19d395.

--- cmake/Modules/Findrestbed.cmake
+++ cmake/Modules/Findrestbed.cmake
@@ -1,11 +1,22 @@
-# Corvusoft has their own Findrestbed.cmake. However, they assume that you 
-# include restbed source with your source. I find this much more reasonable.
-# Just install restbed on your system where you normally would. Update the 
-# paths as needed.
+# Copyright 2014-2016, Corvusoft Ltd, All Rights Reserved.
 
-find_library( restbed_LIBRARY NAMES restbed PATH_SUFFIXES lib PATHS "/usr/local" )
-find_path( restbed_INCLUDE restbed PATH_SUFFIXES include )
+find_path( restbed_SOURCE CMakeLists.txt HINTS "${CMAKE_SOURCE_DIR}/dependency/restbed" )
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(JSONBOX DEFAULT_MSG JSONBOX_LIBRARY JSONBOX_INCLUDE_DIR)
+if ( restbed_SOURCE )
+    set( restbed_FOUND TRUE )
+    set( restbed_BUILD "${CMAKE_CURRENT_BINARY_DIR}/restbed_build" )
+    set( restbed_DISTRIBUTION "${CMAKE_CURRENT_BINARY_DIR}/distribution" )
 
+    include( ExternalProject )
+    ExternalProject_Add( restbed SOURCE_DIR ${restbed_SOURCE}
+                         PREFIX restbed_build
+                         INSTALL_DIR ${restbed_DISTRIBUTION}
+                         CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${restbed_DISTRIBUTION} -DBUILD_SSL=${BUILD_SSL} -DBUILD_SHARED=NO )
+
+    set( restbed_INCLUDE "${restbed_DISTRIBUTION}/include" )
+    set( restbed_LIBRARY "${restbed_DISTRIBUTION}/library/${CMAKE_STATIC_LIBRARY_PREFIX}restbed${CMAKE_STATIC_LIBRARY_SUFFIX}" )
+
+    message( STATUS "${Green}Found Restbed include at: ${restbed_SOURCE}${Reset}" )
+else ( )
+    message( FATAL_ERROR "${Red}Failed to locate Restbed dependency.${Reset}" )
+endif ( )
